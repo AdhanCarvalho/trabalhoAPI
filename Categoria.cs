55e0bb73cb136e80afdfd4abb2b83ae782a37464
AdhanCarvalho <carvalhoadhan16@gmail.com>
2024-06-10 22:34:28 -0300

    .

--- Categoria.cs
+++
@@ -1,6 +0,0 @@
-public class Categoria
-{
-    public int CategoriaId { get; set; }
-    public string Nome { get; set; }
-    public List<Produto> Produtos { get; set; } 
-}
--- Pedido.cs
+++
@@ -1,8 +0,0 @@
-public class Pedido
-{
-    public int PedidoId { get; set; }
-    public DateTime DataPedido { get; set; }
-    public int ClienteId { get; set; } 
-    public Cliente Cliente { get; set; }
-    public List<PedidoItem> Itens { get; set; } 
-}
